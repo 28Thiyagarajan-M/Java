01797beb336bc35c99987368235bedca23f699d0
Thiyagarajan.M <81804719+28Thiyagarajan-M@users.noreply.github.com>
2022-12-12 22:37:48 +0530

    (Insertion, Deletion)

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -36,6 +36,73 @@ public class LinkedList{
         size++;
     }
     
+    public void insertLast(int val){
+        Node node = new Node(val);
+        
+        if(tail == null){
+            insertFirst(val);
+            return;
+        }
+        
+        tail.next  = node;
+        tail = node;
+        size++;
+    }
+    
+    public void insert(int val, int index){
+        
+        if(index == 0){
+            insertFirst(val);
+            return;
+        }
+        
+        if(index == size){
+            insertLast(val);
+        }
+        
+        Node temp = head;
+        
+        for(int i =1; i< index; i++){
+            temp = temp.next;
+        }
+        
+        Node node = new Node(val);
+        node.next = temp.next;
+        temp.next = node;
+        size++;
+    }
+    
+    public int deleteFirst(){
+        int val = head.val;
+        head = head.next;
+        
+        if(head == null){
+            tail = null;
+        }
+        size--;
+        
+        return val;
+                
+    }
+    
+    public int deleteLast(){
+        int val = tail.val;
+        Node secondLast = get(size-2);
+        tail = secondLast;
+        tail.next = null;
+        size--;
+        return val;
+    }
+    
+    public Node get(int index){
+        Node node = head;
+        for(int i = 0; i<index; i++){
+            node = node.next;
+        }
+        
+        return node;
+    }
+    
     
     public void display(){
         Node temp = head;
@@ -46,19 +113,34 @@ public class LinkedList{
         }
         
         System.out.println("End");
+        System.out.println("Size : "+ size);
     }
     
     
     public static void main(String [] args){
         LinkedList ll = new LinkedList();
         
+        System.out.println("Insertion:");
         ll.insertFirst(23);
         ll.insertFirst(5);
         
         ll.insertFirst(34);
         ll.insertFirst(21);
         
+        ll.insertLast(999);
+        
+        ll.insert(28,4);
+        
         ll.display();
+        
+        System.out.println("Deletion:");
+        
+        System.out.println(ll.deleteFirst());
+        
+        ll.display();
+        
+         System.out.println(ll.deleteLast());
+         ll.display();
     }
     
 }
