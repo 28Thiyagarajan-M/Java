9617d468e1e8705d2bb9c8849e9ce131c0b606a8
Thiyagarajan.M <81804719+28Thiyagarajan-M@users.noreply.github.com>
2022-12-13 12:01:38 +0530

    Updated DLL

--- DoublyLinkedList/DoublyLinkedList.java
+++ DoublyLinkedList/DoublyLinkedList.java
@@ -36,6 +36,44 @@ public class DoublyLinkedList {
         head = node;
     }
     
+    public void insertLast(int val){
+        
+        Node node = new Node(val);
+        
+        Node temp = head;
+   
+        
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        
+        temp.next = node;
+        node.prev = temp;
+        node.next = null;
+        
+    }
+    
+    public void insert(int val,int index){
+        
+        if(insert == 0){
+            return insertFirst(val);
+        }
+        
+        Node temp = head;
+        for(int i =1 ; i< index; i++){
+            temp = temp.next;
+        }
+        
+        Node node = new Node(val,temp.next,temp);
+        temp.next = node;
+        
+        // node.next = temp.next;
+        // node.prev = temp;
+        // temp.next = node;
+        
+    }
+    
+    
     public void display(){
         
         Node node = head;
@@ -49,11 +87,11 @@ public class DoublyLinkedList {
         
         System.out.println("End");
         
-        while(last != null){
-            System.out.print(last.val + " - > ");
-            last = last.prev;
-        }
-        System.out.println("Start");        
+        // while(last != null){
+        //     System.out.print(last.val + " - > ");
+        //     last = last.prev;
+        // }
+        // System.out.println("Start");        
     }
     
     
@@ -65,9 +103,18 @@ public class DoublyLinkedList {
         dll.insertFirst(10);
          dll.insertFirst(5);
           dll.insertFirst(30);
-           dll.display();
+          
            dll.insertFirst(20);
             dll.insertFirst(100);
+            
+             
+            dll.insertLast(999);
+            
+            dll.display();
+            
+            dll.insert(2810,2);
+            
+            dll.display();
             
         
        
