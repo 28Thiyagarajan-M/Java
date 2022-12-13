9e882c5369afa9d7493cec35f7a8124f2acdb8b4
Thiyagarajan.M <81804719+28Thiyagarajan-M@users.noreply.github.com>
2022-12-13 10:41:27 +0530

    Includes all operations

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,5 +1,4 @@
-
-public class LinkedList{
+public class LinkedList {
     
     private Node head;
     private Node tail;
@@ -9,19 +8,14 @@ public class LinkedList{
         this.size = 0;
     }
     
+    
     private class Node{
         int val;
         Node next;
         
-        public Node(int val){
+        public Node (int val){
             this.val = val;
         }
-        
-        public Node(int val, Node next){
-            this.val = val;
-            this.next = next;
-        }
-        
     }
     
     public void insertFirst(int val){
@@ -33,10 +27,13 @@ public class LinkedList{
         if(tail == null){
             tail = head;
         }
+        
         size++;
+        
     }
     
     public void insertLast(int val){
+        
         Node node = new Node(val);
         
         if(tail == null){
@@ -44,7 +41,7 @@ public class LinkedList{
             return;
         }
         
-        tail.next  = node;
+        tail.next = node;
         tail = node;
         size++;
     }
@@ -58,45 +55,100 @@ public class LinkedList{
         
         if(index == size){
             insertLast(val);
+            return;
         }
+        
+        
+        Node node= new Node(val);
         
         Node temp = head;
         
-        for(int i =1; i< index; i++){
+        for(int i = 1; i<index;i++){
             temp = temp.next;
         }
         
-        Node node = new Node(val);
         node.next = temp.next;
         temp.next = node;
+        
         size++;
+        
     }
     
     public int deleteFirst(){
+        
         int val = head.val;
+        
         head = head.next;
         
         if(head == null){
             tail = null;
         }
-        size--;
         
+        size--;
         return val;
-                
+        
+    }
+    
+    public void display(){
+        Node temp = head;
+        
+        while(temp != null){
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        
+        System.out.println("End");
+        System.out.println("Size : "+ size);
     }
     
     public int deleteLast(){
         int val = tail.val;
+        
         Node secondLast = get(size-2);
+        secondLast.next = null;
         tail = secondLast;
-        tail.next = null;
+        
         size--;
+        
         return val;
+    }
+    
+    public int delete(int index){
+        
+        if(index == size-1){
+            
+            return deleteLast();
+        }
+        if(index == 0){
+            return deleteFirst();
+        }
+        
+        Node prev = get(index-1);
+        int val = prev.next.val;
+        prev.next = prev.next.next;
+        
+        size --;
+        return val;
+        
+    }
+    
+    public Node find(int val){
+        
+        Node temp = head;
+        while(temp != null){
+            if(temp.val == val){
+                return temp;
+            }
+            temp = temp.next;
+        }
+        
+        return null;
     }
     
     public Node get(int index){
         Node node = head;
-        for(int i = 0; i<index; i++){
+        
+        for(int i = 0; i<index;i++){
             node = node.next;
         }
         
@@ -104,43 +156,32 @@ public class LinkedList{
     }
     
     
-    public void display(){
-        Node temp = head;
-        
-        while(temp != null){
-            System.out.print(temp.val + " -> ");
-            temp = temp.next;  
-        }
-        
-        System.out.println("End");
-        System.out.println("Size : "+ size);
-    }
-    
     
     public static void main(String [] args){
         LinkedList ll = new LinkedList();
         
-        System.out.println("Insertion:");
-        ll.insertFirst(23);
-        ll.insertFirst(5);
+        ll.insertFirst(10);
+        ll.insertFirst(20);
         
-        ll.insertFirst(34);
-        ll.insertFirst(21);
+        ll.insertFirst(230);
+         ll.insertLast(30);
+         ll.insertLast(23);
+         
         
-        ll.insertLast(999);
+        ll.display();
+        ll.displayFirst();
+        ll.display();
+        ll.deleteLast();
+        ll.display();
+        ll.delete(2);
         
-        ll.insert(28,4);
         
         ll.display();
         
-        System.out.println("Deletion:");
-        
-        System.out.println(ll.deleteFirst());
-        
-        ll.display();
-        
-         System.out.println(ll.deleteLast());
-         ll.display();
+       
+         System.out.println(ll.find(30));   
+       
     }
+    
     
 }
